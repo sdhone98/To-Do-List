95a9d39d60d6335ee4cb740370c16135b7e64fbf
Sagar-SankeySolutons <sagar.d@sankeysolutions.com>
2023-07-25 19:52:51 +0530

    Search Bar Added

--- src/components/NavAppBar/NavAppBar.tsx
+++ src/components/NavAppBar/NavAppBar.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';  
-import { AppBar, Toolbar, Button } from "@mui/material";
+import { AppBar, Toolbar, Button, Box } from "@mui/material";
 import { Typography } from "@mui/material";
 import AddIcon from "@material-ui/icons/Add";
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+
+
 
 
 const style = {
@@ -11,28 +19,94 @@ const style = {
     // background: 'blue'
   };
 
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+    },
+}));
+
 interface NavAppBarProps {
     onOpenDialog: () => void;
   }
 
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
+
 
   const NavAppBar: React.FC<NavAppBarProps> = ({ onOpenDialog }) => {
     return (
-      <AppBar component="nav">
-        <Toolbar>
-          <Typography>To Do List</Typography>
-          <Button
-            sx={style}
-            // color="secondary"
-            startIcon={<AddIcon />}
-            variant="contained"
-            size="medium"
-            onClick={onOpenDialog}
-          >
-            Add
-          </Button>
-        </Toolbar>
-      </AppBar>
+
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ display: { xs: 'none', sm: 'block' } }}
+                    >
+                        To Do List
+                    </Typography>
+
+                    <Search>
+                        <SearchIconWrapper>
+                            <SearchIcon />
+                        </SearchIconWrapper>
+                        <StyledInputBase
+                            placeholder="Search…"
+                            inputProps={{ 'aria-label': 'search' }}
+                            onChange={() => { console.log(1)}}
+                        />
+                    </Search>
+
+                    <Button
+                          sx={style}
+                          // color="secondary"
+                          startIcon={<AddIcon />}
+                          variant="contained"
+                          size="medium"
+                          onClick={onOpenDialog}
+                        >
+                          Add
+                        </Button>
+
+                </Toolbar>
+
+            </AppBar>
+
+        </Box>
     );
   };
 
